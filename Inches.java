4c212435fd1e20047cb0a3fce4d7ec89f8eebdb1
Abirami-2401 <108385166+Abirami-2401@users.noreply.github.com>
2022-08-04 19:25:13 +0530

    Add files via upload

---
+++ Inches.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+
+
+public class Inches {
+
+	public static void main(String[] args) {
+		double inches;
+			inches=1000;
+			System.out.println("Enter the value for inches: ");
+		double meters;	
+			meters =0.0254;
+			System.out.println("the number in meter" + meters);
+	}
+
+	
+}
