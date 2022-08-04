2fdade663933c5a364ecb9a309a3dcfa640424a1
Abirami-2401 <108385166+Abirami-2401@users.noreply.github.com>
2022-08-04 18:33:01 +0530

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+public class Temperature {
+	
+	
+
+	public static void main (String args[]){ 
+		float temp;
+		temp=212;
+		System.out.println("Enter the Temperature: ");
+		temp=((temp-32)*5)/9;
+		System.out.println("Celsius temp is" +temp);
+		
+		}
+	}
+
+	
